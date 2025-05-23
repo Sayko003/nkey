c52b1c22de538e77cc6f41d64d2cff5dc6f62d5e
Sayko003 <konstantin.web@bk.ru>
2025-05-23 19:05:37 +0300

    ready blocks

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -18,7 +18,39 @@ const slider = () =>{
             clickable: true,
         },
 
-        });
+    });
+
+    const catalogSwiper = new Swiper('.catalog__swiper', {
+            modules: [Pagination],
+            direction: 'horizontal',
+            autoplay: {
+                delay: 5000,
+            },
+            slidesPerView: 3,
+            pagination: {
+                el: '.catalog__swiper-pagination',
+                clickable: true,
+            },
+            spaceBetween: 30,
+    });
+
+    const reviewsSwiper = new Swiper('.reviews__swiper', {
+            modules: [Navigation,Pagination],
+            direction: 'horizontal',
+            autoplay: {
+                delay: 5000,
+            },
+            slidesPerView: 2,
+            pagination: {
+                el: '.reviews__swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.reviews__swiper-button-next',
+                prevEl: '.reviews__swiper-button-prev',
+            },
+            spaceBetween: 30,
+    });
 }
 
 export default slider;
